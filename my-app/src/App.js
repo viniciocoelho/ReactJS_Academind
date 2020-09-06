df9bfbab26738b395692be08d2e1a96b4797a8a1
Vinicio Coelho <coelhocvcg@gmail.com>
2020-09-06 04:21:17 -0300

    Understanding JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,14 @@ class App extends Component {
         <h1>Hi, I am a React App</h1>
       </div>
     );
+    /*
+    Os dois códigos são a mesma coisa mas escrita de maneiras diferentes
+    A maneira de cima é mais economica e por isso mais usada
+    return React.createElement(
+      "div",
+      null,
+      React.createElement("h1", { className: "App" }, "Hi, I am a React App") 
+    );*/
   }
 }
 
